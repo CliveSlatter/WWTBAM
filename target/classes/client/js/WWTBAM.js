e02d40d5339d7abb73b4bf8f9cd0ddfd8f9fc3b4
CliveSlatter <cslatter@farnborough.ac.uk>
2020-12-22 12:28:33 +0000

    Web cam option added and the money options formatted

--- target/classes/client/js/WWTBAM.js
+++ target/classes/client/js/WWTBAM.js
@@ -5,15 +5,53 @@ function getMoneyTokens(){
     ).then(data => {
         if (data.hasOwnProperty('error')) {
         } else if (data.hasOwnProperty('rewards')) {
-            let moneyHTML = `<table>`;
+            let moneyHTML = `<table id="money">`;
             for(let r of data.rewards){
-                moneyHTML += `<tr>`+
+                moneyHTML += `<tr id="${r.id}" onclick=console.log(this.id);>`+
                     `<td>${r.id}</td>`+
                     `<td>${r.value}</td>`+
                     `</tr>`;
             }
             moneyHTML+=`</table>`;
             document.getElementById("money").innerHTML = moneyHTML;
+            
         }
     });
+}
+
+function handleEvent(e) {
+    console.log("Clicked")
+}
+
+
+function getVideo(){
+    let video = document.querySelector("#videoElement");
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+            });
+    }
+}
+
+function stop(e) {
+    let stream = video.srcObject;
+    let tracks = stream.getTracks();
+
+    for (let i = 0; i < tracks.length; i++) {
+        let track = tracks[i];
+        track.stop();
+    }
+
+    video.srcObject = null;
+}
+
+function runThese(){
+    getVideo();
+    //clickCell();
+    getMoneyTokens();
 }
